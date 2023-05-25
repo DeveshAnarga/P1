762092f0ce6cc751996f1d08fa295b390adc32ba
DeveshAnarga <134617276+DeveshAnarga@users.noreply.github.com>
2023-05-25 23:32:50 +1000

    Add files via upload

---
+++ ForeverOZ/js/custom.js
@@ -0,0 +1,11 @@
+(function ($) {
+$(document).ready(function(){
+ $('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  arrows: false
+});
+});
+})(jQuery);
